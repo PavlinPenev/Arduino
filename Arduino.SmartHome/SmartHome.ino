46ec1489abdfb85db41e18f4780c6bfc679cb82c
Paf <pavlinpenevv@abv.bg>
2025-12-13 01:30:43 +0200

    Project final commit

--- Arduino.SmartHome/SmartHome.ino
+++ Arduino.SmartHome/SmartHome.ino
@@ -1,6 +1,7 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
+#include <DHT.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 volatile int DarknessLevel;
@@ -23,12 +24,39 @@ const int RAIN_TRESHOLD = 100;
 int RainLevel;
 Servo WindowServo; 
 
+const int FAN_PIN = 6;
+const int FLAME_SENSOR_PIN = 8;
+const int ALARM_PIN = 3;
+const int FIRE_LED_PIN = 12;
+int flame;
+
+const int HUMIDITY_TEMP_SENSOR_PIN = 13;
+const int SENSOR_TYPE = 11;
+int temperature;
+int humidity;
+DHT dht12(HUMIDITY_TEMP_SENSOR_PIN, SENSOR_TYPE);
+
+const int DOOR_SERVO_PIN = 9;
+const int RED_BUTTON_PIN = 4;
+const int GREEN_BUTTON_PIN = 5;
+const String password = "GRRG"; 
+const unsigned long INPUT_TIMEOUT = 4000;
+unsigned long lastPressTime = 0;
+Servo DoorServo;
+String passwordInput = "";
+String asterisksInput = "";
+bool prevGreenButtonState;
+bool prevRedButtonState;
+
 void setup()
 {
   runLcd();
   prepareDoorLedJob();  
   prepareParking();
   prepareRainSensor();
+  prepareFlameAlarm();
+  prepareTempHumidityControl();
+  prepareDoorPassword();
 }
 
 
@@ -37,6 +65,9 @@ void loop()
   runDoorLedJob();
   handleCoinInsert();
   handleRainLevel();
+  handleFire();
+  handleTempAndHumidity();
+  handlePasswordButtonPress();
 }
 
 void runLcd() 
@@ -44,9 +75,9 @@ void runLcd()
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Pavkata");
-  lcd.setCursor(0, 1);
-  lcd.print("WeLoveDigi TL");
+  // lcd.print("Pavkata");
+  // lcd.setCursor(0, 1);
+  // lcd.print("WeLoveDigi TL");
 }
 
 void prepareDoorLedJob()
@@ -63,7 +94,7 @@ void runDoorLedJob()
 {
   DarknessLevel = analogRead(PHOTOCELL_PIN);
   Environmental_sound = digitalRead(SOUND_SENSOR_PIN);
-  //Serial.println(DarknessLevel);
+
   if (DarknessLevel > DARKNESS_TRESHOLD) 
   {
     if (Environmental_sound == 1) 
@@ -89,7 +120,6 @@ void prepareParking()
 void handleCoinInsert()
 {
   IsCoinInserted = digitalRead(COIN_SLOT_PIN);
-  Serial.println(IsCoinInserted);
 
   // Sensor gives a 0 value when there is a coin input
   if (IsCoinInserted == 0)
@@ -115,7 +145,7 @@ void prepareRainSensor()
 void handleRainLevel()
 {
   RainLevel = analogRead(RAIN_SENSOR_PIN);
-  Serial.println("Rain Level: " + RainLevel);
+  
   if (RAIN_TRESHOLD < RainLevel)
   {
     WindowServo.write(90);
@@ -124,4 +154,165 @@ void handleRainLevel()
   {
     WindowServo.write(0);
   }
+}
+
+void prepareFlameAlarm()
+{
+  flame = 0;
+  pinMode(FLAME_SENSOR_PIN, INPUT);
+  pinMode(FAN_PIN, OUTPUT);
+  pinMode(FIRE_LED_PIN, OUTPUT);
+  digitalWrite(FAN_PIN, LOW);
+}
+
+void handleFire()
+{
+  flame = digitalRead(FLAME_SENSOR_PIN);
+
+  if (flame == 1)
+  {
+    setFanPower(255);
+    digitalWrite(FIRE_LED_PIN, HIGH);
+    tone(ALARM_PIN, 3532);
+  }
+  else
+  {
+    setFanPower(0);
+    digitalWrite(FIRE_LED_PIN, LOW);
+    noTone(ALARM_PIN);
+  }
+}
+
+void prepareTempHumidityControl()
+{
+  temperature = 0;
+  humidity = 0;
+  dht12.begin();
+}
+
+void handleTempAndHumidity()
+{
+  int currentTemp = dht12.readTemperature();
+  int currentHumidity = dht12.readHumidity();
+
+  if(currentTemp != temperature || currentHumidity != humidity)
+  {
+    Serial.println("In Display temp humidity");
+    lcd.clear();
+    temperature = currentTemp;
+    humidity = currentHumidity; 
+    printTempAndHumidity();
+  }
+
+  if(temperature > 30)
+  {
+    setFanPower(255);
+  } else {
+    setFanPower(0);
+  }
+}
+
+void setFanPower(int speed)
+{
+  if (speed <= 0)
+  {
+    analogWrite(FAN_PIN, 0);
+  }
+  else if (speed > 255)
+  {
+    analogWrite(FAN_PIN, 255);
+  }
+  else
+  {
+    analogWrite(FAN_PIN, speed);
+  }
+}
+
+void prepareDoorPassword()
+{
+  pinMode(GREEN_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(RED_BUTTON_PIN, INPUT_PULLUP);
+  DoorServo.attach(DOOR_SERVO_PIN);
+  DoorServo.write(75);
+}
+
+void handlePasswordButtonPress()
+{
+  bool isGreenPressed = digitalRead(GREEN_BUTTON_PIN);
+  bool isRedPressed = digitalRead(RED_BUTTON_PIN);
+
+  if (isGreenPressed == LOW && prevGreenButtonState == HIGH)
+  {
+    pressButton('G');
+  }
+
+  if (isRedPressed == LOW && prevRedButtonState == HIGH)
+  {
+    pressButton('R');
+  }
+
+  prevGreenButtonState = isGreenPressed;
+  prevRedButtonState = isRedPressed;
+}
+
+void pressButton(char button)
+{
+  tone(ALARM_PIN, 2000, 80);
+  passwordInput += button;
+  asterisksInput += "*";
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  
+  if (passwordInput.length() < password.length())
+  {
+    lcd.print("Password:");
+    lcd.setCursor(0, 1);
+    lcd.print(asterisksInput);
+  }
+  else 
+  {
+    if (password == passwordInput)
+    {
+      lcd.print("Password");
+      lcd.setCursor(0, 1);
+      lcd.print("correct!");
+      DoorServo.write(180);
+      delay(3000);
+      DoorServo.write(75);
+    }
+    else
+    {
+      lcd.print("Password");
+      lcd.setCursor(0, 1);
+      lcd.print("incorrect!");
+      delay(2000);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Password:");
+      lcd.setCursor(0, 1);
+      lcd.print(asterisksInput);
+    }
+
+    resetInput();
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    printTempAndHumidity();
+  }
+}
+
+void resetInput()
+{
+  passwordInput = "";
+  asterisksInput = "";
+}
+
+void printTempAndHumidity()
+{
+  lcd.print("Temperature: ");
+  lcd.print(temperature);
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: ");
+  lcd.print(humidity);
 }
